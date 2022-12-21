400f49a3be2602f3703eeba7726375c31051ecf4
Chaiwatintharatun <s6406021620017@email.kmutnb.ac.th>
2022-12-21 11:04:44 +0700

    update

---
+++ 2/nummath.js
@@ -0,0 +1,19 @@
+let num1 = 100;
+
+//Basic Math
+console.log("********Basic Math********");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+
+console.log("********Additional Mathematical Operations********");
+//Addtional Mathematical Operations
+console.log(num1 % 1500); // Reminder
+console.log(++num1); //increment
+console.log(--num1); //Decrement
+
+console.log("********Using the Main Object********");
+//Using the Math Object
+console.log(Math.PI); //Pi
+console.log(Math.sqrt(num1)); //Square root
