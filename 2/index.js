b7f8711ecd8fe02ba453a4b9fee016d9fdc37c03
Chaiwatintharatun <s6406021620017@email.kmutnb.ac.th>
2022-12-21 09:42:44 +0700

    update

---
+++ 2/index.js
@@ -0,0 +1,11 @@
+const message = "Hello";
+const place = "World";
+
+// logging out a string
+console.log('Hello, World!');
+
+// using substitutions
+console.log('Hello, %s!', Place);
+
+// using a string literal
+console.log('${message}, ${place}!');
