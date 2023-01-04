5b1ae9c2471c4ce54cd0410add91597a9caee805
Chaiwatintharatun <s6406021620017@email.kmutnb.ac.th>
2023-01-04 10:09:21 +0700

    update

---
+++ arrayReducer.js
@@ -0,0 +1,10 @@
+const array = [15, 16, 17, 18, 19];
+
+function reducer(accmulator, currentValue, index) {
+    const return = accmulator + currentValue;
+    console.log(
+        'accmulator: ${accmulator}, currentValue; ${currentValue}, index: ${index}, returns; ${return}',);
+        return returns;
+    }
+
+array.reduce(reduce);
---
+++ reduce.js
@@ -0,0 +1,6 @@
+const array0Numbers = [1, 2, 3, 4];
+const sum = array0Numbers.reduce((accmulator, currentValue) => {
+    return accmulator + currentValue;
+});
+
+console.log(sum); //10
